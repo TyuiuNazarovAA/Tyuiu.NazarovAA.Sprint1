46e3cc5214694ac54cd39a99d79221a7248287f8
NazarovAA <Tyuiu.NazarovAA@outlook.com>
2025-09-11 16:56:36 +0500

    Add project files.

---
+++ Tyuiu.NazarovAA.Sprint1.Task0.V5.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.NazarovAA.Sprint1.Task0.V5.Lib;
+
+namespace Tyuiu.NazarovAA.Sprint1.Task0.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalculationValid()
+        {
+            DataService ds = new DataService();
+            double result = ds.Calculate();
+            Assert.AreEqual(12, result);
+        }
+    }
+}
