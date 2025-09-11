c544c496b5318d344dbe09a47d66fba5d42a9f92
NazarovAA <Tyuiu.NazarovAA@outlook.com>
2025-09-11 18:07:53 +0500

    Task1

---
+++ Tyuiu.NazarovAA.Sprint1.Task1.V30.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.NazarovAA.Sprint1.Task1.V30.Lib;
+
+namespace Tyuiu.NazarovAA.Sprint1.Task1.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalculationValid()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(3, ds.Calculate(4));
+        }
+    }
+}
---
+++ Tyuiu.NazarovAA.Sprint1.Task1.V30/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.NazarovAA.Sprint1.Task1.V30.Lib;
+
+namespace Tyuiu.NazarovAA.Sprint1.Task1.V30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Назаров А. А. | РППб 25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Выполнил: Назаров Артём Александрович | СППб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле (2+x)/2 и печатает его на экране.        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x));
+        }
+    }
+}
