b94cab09ff189037a9e160b7f98ca468c77ab9b6
NazarovAA <Tyuiu.NazarovAA@outlook.com>
2025-09-12 12:24:56 +0500

    Task4

---
+++ Tyuiu.NazarovAA.Sprint1.Task4.V10.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.NazarovAA.Sprint1.Task4.V10.Lib
+{
+    public class DataService : ISprint1Task4V10
+    {
+        public double Calculate(double a)
+        {
+            return Math.Round((1 + Math.Cos(a)) / Math.Pow(Math.Sin(a), 2), 3);
+        }
+    }
+}
---
+++ Tyuiu.NazarovAA.Sprint1.Task4.V10.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.NazarovAA.Sprint1.Task4.V10.Lib;
+
+namespace Tyuiu.NazarovAA.Sprint1.Task4.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalculationValid()
+        {
+            DataService ds = new DataService();
+            double result = ds.Calculate(1);
+            double wait = 2.175;
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
